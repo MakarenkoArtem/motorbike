9ab41dedc5e33409656bd2c6fd43ead6539107e5
artem <makarenkoiandeks@yandex.ru>
2024-08-25 23:41:28 +0300

    update BTSerial

--- arduino/Bike/BTSerial.cpp
+++ arduino/Bike/BTSerial.cpp
@@ -5,74 +5,113 @@
 #include "BTSerial.h"
 
 BTSerial::BTSerial(int RX, int TX) : SoftwareSerial(RX, TX) {
-    this->begin(9600);
+    this->begin(SPEED);
     this->setTimeout(TIMEOUT);
     timer=millis();
 }
 
-short BTSerial::getSocket(byte &bright, unsigned short &curMode, byte (&colors)[24]) {
-    if (sz!=this->available() || !sz){
-        sz=this->available();
-        return OK;//(timer>millis())?WAIT_INPUT:OK;
-    }   
-    Serial.print(sz!=this->available());
-    Serial.print(" ");
-    Serial.print(!sz);
-    Serial.print(" ");
+short BTSerial::getSocket(byte &bright, unsigned short &curMode, byte (&colors)[24], byte &frequency) {
+    /*не ставить тайм ауты, тк по умолчанию размер буфера составляет 64 байта и при переполнении старые данные будут зетерты
+    if(!available() || (sz==-1 && millis()-timer<100)){//если ввод пуст или время с начала ввода меньше тайм аута
+        return OK;      
+    } else if(sz==-2){ //если начался прием
+        sz=-1;
+        timer = millis();
+        return OK;     
+    } else*/
+    if(!available()){
+        return OK;
+    }
+    if (millis()-timer > DELAY){ //если пора очищать ввод
+        sz=-1;
+    }
+    if (sz==-1) {
+        timer = millis();
+    }
+     /*
+    Serial.print("Size:");
     Serial.print(sz);
-    Serial.print(" ");
-    Serial.println(available());
-    //this->flush();//ожидать конца передачи данных
-    sz = -1;
-    *buf=0;
+    Serial.print(" buf:");
+    Serial.print(buf);
+    Serial.print(" avaliable:");
+    Serial.println(available());*/
     do {
         buf[++sz] = this->read();
-    } while (this->available() and buf[sz] and sz <= MAXSZ);
-    /*if (!this->available() && buf[sz]){
-        timer=millis()+delay;
-        return WAIT_INPUT;
-    }*/
-    if (MAXSZ < sz) {
-        return ERROR;
+    } while (available() and buf[sz]!='\n' and sz < MAXSZ);
+    if (MAXSZ == sz) {
+        sz=-1;
+        return ERROR; 
     }
+    if(buf[sz]=='\n'){
+        buf[sz]=0;
+    }else{
+        buf[sz+1]=0;
+        /*Serial.print(F("Part: "));
+        Serial.print(buf);
+        Serial.print(" avaliable:");
+        Serial.println(available());*/
+        timer = millis()-TIMEOUT;
+        return WAIT_INPUT;
+    }
+    Serial.print(F("Command:"));
+    Serial.print(buf);
+    Serial.print(F(" size:"));
+    Serial.print(sz);
+    Serial.print(F(" avaliable:"));
+    Serial.println(available());
     short ans = OK;
-    if (compareStr(buf, "OFF")) {
-        this->println(F("OK"));
+    if (compareStr(buf, "GC")) {
+          for (int x = 0; x < 6; ++x) {
+                this->print(colors[x * 4]);
+                this->print(F(","));
+                this->print(colors[x * 4 + 1]);
+                this->print(F(","));
+                this->print(colors[x * 4 + 2]);
+                this->print(F(","));
+                this->print(colors[x * 4 + 3]);
+                this->print(F(","));
+          }
+    }else if (compareStr(buf, "Con")) {
+        this->print(F("OK"));
+    } else if (compareStr(buf, "OFF")) {
+        this->print(F("OK"));
         ans = OFF;
     } else if (compareStr(buf, "ON")) {
+        this->print(F("OK"));
         ans = ON;
+    } else if (compareStr(buf, "LOW")) {
+        this->print(F("OK"));
+        ans = SOUND_OFF;
+    } else if (compareStr(buf, "HIGH")) {
+        this->print(F("OK"));
+        ans = SOUND_ON;
     } else if (compareStr(buf, "END")) {
         ans = END;
     } else {
         char *t = subStr(buf, 0, 3);
         if (compareStr(t, "Br:")) {
             bright = static_cast<byte>(strToLongInt(buf+3));
+            ans = BRIGHT;
         } else if (compareStr(t, "Ty:")) {
-            curMode = static_cast<unsigned short >(strToLongInt(buf+3));
-        } else if (compareStr(t, "Co:") && sz == 99) {
+            curMode = static_cast<unsigned short>(strToLongInt(buf+3));
+            ans = MODE;
+        } else if (compareStr(t, "CF:")) {
+            frequency = static_cast<unsigned short>(strToLongInt(buf+3));
+            ans = FREQUENCY;
+        } else if (compareStr(t, "Co:")){
+            if(sz != 99) {
+                Serial.println(F("Damaged message"));
+            }
             char *val;
             for (int i = 0; i < 24; ++i) {
                 val = subStr(buf+3 + i * 4, 0, 3);
                 colors[i] = static_cast<byte>(strToLongInt(val));
                 free(val);
             }
-            //timer += DELAY;
-        } else if (compareStr(t, "Con")) {
-            this->println(F("OK "));
-            for (int x = 0; x < 6; ++x) {
-                this->print(colors[x * 4]);
-                this->print(F(", "));
-                this->print(colors[x * 4 + 1]);
-                this->print(F(", "));
-                this->print(colors[x * 4 + 2]);
-                this->print(F(", "));
-                this->print(colors[x * 4 + 3]);
-                this->print(F(", "));
-            }
-            //timer += DELAY;
-        }// else { BTtimer += DELAY_BT; }
+            ans = COLORS;
+        }
         free(t);
     }
-    Serial.println(buf);
+    sz=-1;
     return ans;
 }
--- arduino/Bike/BTSerial.h
+++ arduino/Bike/BTSerial.h
@@ -15,19 +15,26 @@
 #define BRIGHT 4
 #define TYPE 5
 #define COLORS 6
+#define SOUND_OFF 7
+#define SOUND_ON 8
+#define BRIGHT 9
+#define MODE 10
+#define FREQUENCY 11
 #define WAIT_INPUT 101
 
-#define TIMEOUT 300//200
+#define TIMEOUT 200//200
+#define SPEED 9600
 #define DELAY 1000
 #define MAXSZ 100
 
+
 class BTSerial : public SoftwareSerial {
     char buf[MAXSZ];
-    unsigned short sz = 0;
+    short sz = -1;
     unsigned long int timer;
     
 public:
     BTSerial(int RX, int TX);
 
-    short getSocket(byte &bright, unsigned short &curMode, byte (&colors)[24]);
+    short getSocket(byte &bright, unsigned short &curMode, byte (&colors)[24], byte &frequency);
 };
