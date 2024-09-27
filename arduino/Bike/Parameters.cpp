eb514aa7b64290df5af9a4e6da435764844b9c01
artem <makarenkoiandeks@yandex.ru>
2024-09-27 21:51:33 +0300

    creating an object to pass Parameters

---
+++ arduino/Bike/Parameters.cpp
@@ -0,0 +1,12 @@
+#include "Parameters.h"
+
+Parameters::Parameters(RGBLine &line) :
+        bright(line.bright), mode(line.mode),
+        colors(line.colors), frequency(line.frequency) {
+
+};
+
+Parameters::Parameters(byte bright, unsigned short mode, byte (&colors)[24], byte frequency) :
+        bright(bright), mode(mode),
+        colors(colors), frequency(frequency) {}
+
---
+++ arduino/Bike/Parameters.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <Arduino.h>
+
+#include "RGBLine.h"
+
+class Parameters {
+public:
+    byte bright;
+    unsigned short mode;
+    byte frequency;
+    byte *colors;
+
+    Parameters(RGBLine &line);
+
+    Parameters(byte bright, unsigned short mode, byte (&colors)[24], byte frequency);
+};
