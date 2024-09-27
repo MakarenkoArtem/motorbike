a527de9eeecd32ce21a1e1a37d5005d276083799
artem <makarenkoiandeks@yandex.ru>
2024-09-27 21:52:49 +0300

    decorating code IgnitionKey

--- arduino/Bike/IgnitionKey.cpp
+++ arduino/Bike/IgnitionKey.cpp
@@ -4,15 +4,14 @@
 
 #include "IgnitionKey.h"
 
-IgnitionKey::IgnitionKey(int pin, void (*pinMode)(int, int),
-                         void (*digitalWrite)(int, int)) : pin(pin), pinMode(pinMode),
-                                                           digitalWrite(digitalWrite) {
-    pinMode(pin, 0x1);
-    digitalWrite(pin, val ? 0x1 : 0x0);
+IgnitionKey::IgnitionKey(int pin, void (*pinMode)(int, int), void (*digitalWrite)(int, int)) :
+        pin(pin), pinMode(pinMode), digitalWrite(digitalWrite) {
+    pinMode(pin, HIGH);
+    digitalWrite(pin, val ? HIGH : LOW);
 }
 
 
 void IgnitionKey::setVal(bool val) {
     this->val = val;
-    digitalWrite(pin, val ? 0x1 : 0x0);
+    digitalWrite(pin, val ? HIGH : LOW);
 }
