851a40772db745aa5c3bd5f02004510882e8cf74
artem <makarenkoiandeks@yandex.ru>
2025-02-01 21:46:52 +0300

    Global update methods rendering.Adding logic file- Animation

--- arduino/Bike/RGBLine.h
+++ arduino/Bike/RGBLine.h
@@ -1,84 +1,44 @@
 #pragma once
 
 #include <FastLED.h>
-
+#include "Parameters.h"
 #include "config.h"
 
-#ifndef CONFIG
-#define MIN_STROBE_PERIOD 150
-#endif
-
-extern float fStubLink;
-extern byte bStubLink;
-
 class RGBLine {
-    unsigned short oldMode = 11;
     const int pin;
     CRGBPalette32 myPal;
-    float &sound = fStubLink;
-    long int strobePeriod = MIN_STROBE_PERIOD;
     int STROBE_SAT = 255;
-    byte strobeBright = 0;
     byte id;
-    byte hue = 0;
-    byte hueStep = 2;
-    int hueSpeed = 3;
-    int hueTimer = millis();
-public:
-    bool needAmplitude = false;
-    byte *colors = &bStubLink;
-    CRGB *line;
-    byte maxBright = 0;
-    int count;
-    unsigned short mode = 11;
-    byte frequency = 0;
+    Parameters &params;
     CFastLED *fastLED = nullptr;
+public:
+    int count;
+    CRGB *line;
 
-    RGBLine(int pin, int count, byte *colors, byte id);
+    RGBLine(int pin, int count, Parameters &params, byte id);
 
     void setFastLED(CFastLED *fastLED);
 
     int getPin();
 
-    void setMode(unsigned short mode);
-
-    void setMaxBrightness(byte bright);
-
     void setBrightness(byte bright);
 
-    void setColors(byte *newColors);
-
-    void setFrequency(byte frequency);
-
-    void changeMode();
-
-    void regGradient();
-
-    void strobeHSV();
-
-    void strobe();
-
-    void moveEffect();
-
-    void regHSV();
-
-    void show(float amplitude);
+    void setColors(byte *colors);
 
     void show();
 
-    void regAA();
-
-    void regAB();
-
-    void regDA();
-
-    void changeGradientAB();
-
-    void changeGradientAC();
-
-    void changeGradientAD();
-
-    void blick();
-
     void data();
+//verified 1.02.25
+private:
+    byte calculatePhase(byte phase, int index);
+
+    void renderStaticPattern();
+
+    byte calculatePhaseByAmplitude(byte amplitude, int index);
+
+    void renderFlashByAmplitude(byte amplitude);
+
+    void renderRunningFlashByAmplitude(int countAmp, byte *amplitudes);
+
+    void renderColumn(byte amplitude);
 };
