4fb537c1bd08f78968bc30bbc49df0bf3269e40a
Artem <makarenkoiandeks@yandex.ru>
2025-03-18 23:30:30 +0300

    improving the presentation of methods in BluetoothService

--- android/app/src/main/java/com/example/bike/services/bluetooth/BluetoothService.kt
+++ android/app/src/main/java/com/example/bike/services/bluetooth/BluetoothService.kt
@@ -5,45 +5,28 @@ import android.app.Service
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
-import android.util.Log
-import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.example.bike.domain.model.Device
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import java.util.UUID
 
 class BluetoothService: Service() {
     private var bluetoothAdapter: BluetoothAdapter? = null
-
-    private val _devicesFlow: MutableStateFlow<List<BluetoothDevice>> =
+    private val _bluetoothDevicesFlow: MutableStateFlow<List<BluetoothDevice>> =
         MutableStateFlow(emptyList())
-    private val devicesFlow: StateFlow<List<BluetoothDevice>> = _devicesFlow
+    private val _devicesFlow: MutableStateFlow<List<Device>> = MutableStateFlow(emptyList())
+    val devicesFlow: StateFlow<List<Device>> = _devicesFlow
 
     override fun onCreate() {
         super.onCreate()
-        updateAdapter().onFailure {
-            Toast.makeText(this, it.message ?: it.toString(), Toast.LENGTH_SHORT
-            ).show()
-            return
-        }
-        updateBondedDevices().onFailure {
-            Log.d("BikeBluetooth", it.message ?: it.toString())
-            Toast.makeText(this, it.message ?: it.toString(), Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-
-    private fun updateAdapter(): Result<Unit> {
-        bluetoothAdapter = getSystemService(BluetoothManager::class.java)?.adapter
-        return if (bluetoothAdapter != null) {
-            Result.success(Unit)
-        } else {
-            Result.failure(Exception("Bluetooth adapter not found"))
-        }
+        getStatus()
     }
 
     private val binder = LocalBinder()
@@ -56,82 +39,100 @@ class BluetoothService: Service() {
         return binder
     }
 
+    fun getStatus(): Result<Unit> = kotlin.runCatching {
+        checkAdapter().getOrThrow()
+        updateBondedDevices().getOrThrow()
+    }
+
+    private fun updateAdapter(): Result<Unit> = kotlin.runCatching {
+        bluetoothAdapter = getSystemService(BluetoothManager::class.java)?.adapter
+        if (bluetoothAdapter == null) {
+            throw Exception("Bluetooth adapter not found")
+        }
+    }
+
     fun getRequiredPermissions(): Array<String> {
         return when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE -> arrayOf(Manifest.permission.BLUETOOTH_CONNECT,
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.BLUETOOTH_ADVERTISE,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE -> arrayOf(
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
             ) // Android 14+ (API 34+)
 
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> arrayOf(
-                    Manifest.permission.BLUETOOTH_CONNECT,
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.ACCESS_FINE_LOCATION,
             ) // Android 12-13 (API 31-33)
 
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> arrayOf(Manifest.permission.ACCESS_FINE_LOCATION
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION
             ) // Android 10-11 (API 29-30)
 
             else -> emptyArray() // Для Android 9 и ниже разрешения не нужны
         }
     }
 
-    fun checkBluetoothPermission(): Result<Unit> {
+    fun checkBluetoothPermission(): Result<Unit> = kotlin.runCatching {
         val permissions = getRequiredPermissions()
         val answer = permissions.map {permission ->
-            ContextCompat.checkSelfPermission(this, permission
+            ContextCompat.checkSelfPermission(
+                this, permission
             ) == PackageManager.PERMISSION_GRANTED
         }
-        if (answer.all {true}) {
-            return Result.success(Unit)
-        }
-        return Result.failure(Exception("Does not have permissions: " + answer.mapIndexed {ind, it ->
-            if (it) {
-                ""
-            } else {
-                permissions[ind]
+        if (answer.any {false}) {
+            throw Exception("Does not have permissions: " + answer.mapIndexedNotNull {ind, it ->
+                if (it) {
+                    null
+                } else {
+                    permissions[ind]
+                }
             }
-        }.filter {it != ""}.joinToString(", ")))
+                .joinToString(", "))
+        }
     }
 
-    fun checkAdapter(): Result<Unit> {
-        if (bluetoothAdapter == null || !bluetoothAdapter!!.isEnabled) {
-            updateAdapter()
-        }
+    fun checkAdapter(): Result<Unit> = kotlin.runCatching {
         if (bluetoothAdapter == null) {
-            return Result.failure(IllegalStateException("The device does not have Bluetooth"))
+            updateAdapter().getOrThrow()
         }
         if (!bluetoothAdapter!!.isEnabled) {
-            return Result.failure(IllegalStateException("Bluetooth is turned off"))
+            throw IllegalStateException("Bluetooth is turned off")
         }
-        return Result.success(Unit)
     }
 
-
-    fun getDevicesFlow(): StateFlow<List<BluetoothDevice>> {
+    fun getDevicesFlow(): StateFlow<List<Device>> {
         updateBondedDevices()
         return devicesFlow
     }
 
-    fun updateBondedDevices(): Result<Unit> {
-        checkAdapter().onFailure {
-            Log.d("BikeBluetooth", it.toString())
-            return Result.failure(it)
-        }
-        checkBluetoothPermission().onFailure {return Result.failure(it)}
+    private fun updateBondedDevices(): Result<Unit> = kotlin.runCatching {
+        checkAdapter().getOrThrow()
+        checkBluetoothPermission().getOrThrow()
         val bondedDevices: Set<BluetoothDevice> = bluetoothAdapter!!.bondedDevices
-        bondedDevices.forEach() {item ->
-            Log.d("BikeBluetooth", "${item.name}: ${item.address}")
-        }
-        _devicesFlow.value = bondedDevices.toList()
-        return Result.success(Unit)
+        _bluetoothDevicesFlow.value = bondedDevices.toList()
+        _devicesFlow.value = bondedDevices.mapNotNull {it -> getDevice(it).getOrNull()}
     }
 
-    fun getClient(device: BluetoothDevice): Result<BluetoothClient> {
-        val client = BluetoothClient(device = device)
-        client.connect(true).onFailure {it -> return Result.failure<BluetoothClient>(it)}
-        return Result.success(client)
+    fun getClient(device: Device): Result<BluetoothClient> = kotlin.runCatching {
+        checkBluetoothPermission().getOrThrow()
+        val device = _bluetoothDevicesFlow.value.filter {getDevice(it).getOrNull() == device}
+            .getOrNull(0) ?: throw Exception("Device not found")
+        val bTSocket: BluetoothSocket =
+            device.createInsecureRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")) //незащищенное соединение
+        bTSocket.connect()
+        val client = BluetoothClient(
+            address = device.address,
+            bTSocket = bTSocket,
+            name = device.name
+        )
+        return@runCatching client
+    }
+
+    private fun getDevice(device: BluetoothDevice): Result<Device> = kotlin.runCatching {
+        checkBluetoothPermission().getOrThrow()
+        return@runCatching Device(address = device.address, name = device.name)
     }
 }
