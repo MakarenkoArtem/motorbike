dd48406bb00919b68c4ed5e3645127f75a7f92be
Artem <makarenkoiandeks@yandex.ru>
2025-04-06 21:24:35 +0300

    cleaning after creating component BTDeviceItem

--- android/app/src/main/java/com/example/bike/ui/screens/DeviceListScreen.kt
+++ android/app/src/main/java/com/example/bike/ui/screens/DeviceListScreen.kt
@@ -1,30 +1,21 @@
 package com.example.bike.ui.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Colors
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -33,8 +24,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bike.R
 import com.example.bike.domain.model.Device
-import com.example.bike.domain.model.DeviceStatus
 import com.example.bike.model.ListDeviceViewData
+import com.example.bike.ui.components.BTDeviceItem
 import com.example.bike.ui.components.BluetoothHeaderFragment
 
 @Composable
@@ -102,36 +93,7 @@ fun Body(
             .fillMaxWidth()
             .padding(top = 16.dp),
     ) {
-        itemsIndexed(devices) {ind, device ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {selectionFunc(device)},
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    modifier = Modifier
-                        .heightIn(min = 40.dp)
-                        .wrapContentHeight(Alignment.CenterVertically), //.padding(vertical = 8.dp)
-                    textAlign = TextAlign.End, style = TextStyle(
-                        fontSize = 18.sp
-                    ), text = device.name
-                )
-                if (device.status == DeviceStatus.DISCOVERING) {
-                    CircularProgressIndicator(
-                        modifier = Modifier.size(22.dp), strokeWidth = 2.dp, color = Color.White
-                    )
-                } else if (device.status == DeviceStatus.CONNECTED) {
-                    Icon(
-                        painter = painterResource(R.drawable.checkmark),
-                        contentDescription = "checkMark",
-                        tint = Color.Unspecified, // Оставляет оригинальные цвета
-                        modifier = Modifier.size(22.dp)
-                    )
-                }
-            }
-        }
+        items(devices) {device -> BTDeviceItem(device, selectionFunc)}
     }
 
 }
