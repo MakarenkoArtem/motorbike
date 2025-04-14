3d9634a72d82b76812900a1314b1472101ee79ab
Artem <makarenkoiandeks@yandex.ru>
2025-04-14 15:52:47 +0300

    remove extra functions

--- arduino/Bike/SoundLevelMeter.cpp
+++ arduino/Bike/SoundLevelMeter.cpp
@@ -1,12 +1,9 @@
 #include "SoundLevelMeter.h"
 
-void setADCPrescaler(uint8_t prescaler) {
-    ADCSRA = (ADCSRA & 0b11111000) | prescaler;
-}
 
 SoundLevelMeter::SoundLevelMeter(int pinR, int pinL, void (*pinMode)(int, int),
                                  int (*analogRead)(int))
-        : pinR(pinR), pinL(pinL), pinMode(pinMode), analogRead(analogRead) {
+    : pinR(pinR), pinL(pinL), pinMode(pinMode), analogRead(analogRead) {
     pinMode(pinR, INPUT);
     pinMode(pinL, INPUT);
 }
@@ -75,10 +72,10 @@ void SoundLevelMeter::amplitudeUpdate() {
     } else { smoothedAmplitude = currentAmplitude; }
 }
 
-byte SoundLevelMeter::getExpLikeAmplitude(byte coef) {// значения должны быть в рамках [0, 100]
+byte SoundLevelMeter::getExpLikeAmplitude(byte coef) { // значения должны быть в рамках [0, 100]
     amplitudeUpdate();
     //задачем основание показательной функции в пределах [1.1, 1.9]
-    float e = 1.1+coef*0.008;
+    float e = 1.1 + coef * 0.008;
     float ampl = map(pow(e, static_cast<float>(currentAmplitude) * 20 / 255), 1, pow(e, 20), 0, 255);
     if (ampl < expLikeAmplitude) {
         if (expLikeAmplitude > 20) { expLikeAmplitude -= 20; } else { expLikeAmplitude = 0; }
@@ -101,47 +98,6 @@ byte SoundLevelMeter::getLevelAmplitude() {
 byte SoundLevelMeter::getSmoothedAmplitude() {
     amplitudeUpdate();
     return smoothedAmplitude;
-} //verified 1.02.25
-void SoundLevelMeter::fhtSound() {
-    int RcurrentLevel, LcurrentLevel;
-    setADCPrescaler(
-            ADC_PRESCALER_2); //изменение времени анализа напряжения, сама операция 3 машинных операции(analogRead ~200мо)
-    for (int i = 0; i < 100; i++) { // делаем 100 измерений
-        RcurrentLevel = analogRead(pinR); // с правого
-        LcurrentLevel = analogRead(pinL); // и левого каналов
-        if (RsoundLevel < RcurrentLevel)
-            RsoundLevel = RcurrentLevel; // ищем максимальное
-        if (LsoundLevel < LcurrentLevel)
-            LsoundLevel = LcurrentLevel; // ищем максимальное
-    } // фильтруем по нижнему порогу шумов
-    setADCPrescaler(ADC_PRESCALER_16);
-    RsoundLevel = max(0, map(RsoundLevel, LOW_PASS, 1023, 0, 500));
-    LsoundLevel = max(0, map(LsoundLevel, LOW_PASS, 1023, 0, 500));
-
-    // возводим в степень (для большей чёткости работы)
-    RsoundLevel = pow(RsoundLevel, EXP);
-    LsoundLevel = pow(LsoundLevel, EXP);
-
-    // фильтр
-    RsoundLevel_f = RsoundLevel * SMOOTH + RsoundLevel_f * (1 - SMOOTH);
-    LsoundLevel_f = LsoundLevel * SMOOTH + LsoundLevel_f * (1 - SMOOTH);
-
-    if (RsoundLevel_f > 15 || LsoundLevel_f > 15) {
-        // расчёт общей средней громкости с обоих каналов, фильтрация.
-        // Фильтр очень медленный, сделано специально для автогромкости
-        averageLevel = (RsoundLevel_f + LsoundLevel_f) / 2 * AVERK + averageLevel * (1 - AVERK);
-
-        // принимаем максимальную громкость шкалы как среднюю, умноженную на некоторый коэффициент MAX_COEF
-        maxLevel = averageLevel * MAX_COEF;
-        // преобразуем сигнал в длину ленты (где NUM_LEDS это половина количества светодиодов)
-        Rlenght = map(RsoundLevel_f, 0, maxLevel, 0, 100);
-        Llenght = map(LsoundLevel_f, 0, maxLevel, 0, 100);
-        Serial.print(F("Rlenght:"));
-        Serial.println(Rlenght);
-        // ограничиваем до макс. числа светодиодов
-        Rlenght = constrain(Rlenght, 0, 100);
-        Llenght = constrain(Llenght, 0, 100);
-    }
 }
 
 void SoundLevelMeter::whichCurrentLevel(int curVal) {
@@ -156,4 +112,4 @@ void SoundLevelMeter::whichCurrentLevel(int curVal) {
 void SoundLevelMeter::whichAvegareTimeLight(int avegareTimeLight) {
     Serial.print("Time interval:");
     Serial.println(avegareTimeLight);
-}
+} //verified 9.04.25
--- arduino/Bike/SoundLevelMeter.h
+++ arduino/Bike/SoundLevelMeter.h
@@ -5,15 +5,6 @@
 
 #include "config.h"
 
-//#define DEBUG_SOUND
-//============варианты предделителя(количество тактов для analogRead)========
-#define ADC_PRESCALER_2 0x01    //лучшая скорость, но малое качество
-#define ADC_PRESCALER_4 0x02
-#define ADC_PRESCALER_8 0x03
-#define ADC_PRESCALER_16 0x04
-#define ADC_PRESCALER_32 0x05
-#define ADC_PRESCALER_64 0x06
-#define ADC_PRESCALER_128 0x07  //default лучшее качество, но малая скорость
 //===================================================================
 #define AVERK 0.006
 #define SMOOTH 0.5          // коэффициент плавности анимации VU (по умолчанию 0.5)
@@ -22,7 +13,6 @@
 #define EXP 2.7             // степень усиления сигнала (для более "резкой" работы) (по умолчанию 1.4)
 
 class SoundLevelMeter {
-
     int pinR, pinL;
 
     void (*pinMode)(int, int);
@@ -60,10 +50,5 @@ public:
 
     void whichAvegareTimeLight(int avegareTimeLight);
 
-//verified 11.02.25
-    float LsoundLevel, RsoundLevel;
-    float LsoundLevel_f, RsoundLevel_f;
-    float maxLevel;
-    short Rlenght, Llenght;
-    void fhtSound();
+    //verified 9,04.25
 };
