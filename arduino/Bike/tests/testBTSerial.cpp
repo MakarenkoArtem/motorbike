969ddbbfccea5e8bd8984ef12997c8103a06138b
Makarenko <makarenkoiandeks@yandex.ru>
2025-11-09 23:30:31 +0300

    First worked pipeline

--- arduino/Bike/tests/testBTSerial.cpp
+++ arduino/Bike/tests/testBTSerial.cpp
@@ -9,7 +9,7 @@ void fillInput(BTSerial &bt, const char *cmd) {
     }
 }
 
-void tests(BTSerial &bt, Parameters &params, char *text, short answer) {
+void tests(BTSerial &bt, Parameters &params,const char *text, short answer) {
     std::cout << "Input: "<< text << std::endl;
     fillInput(bt, text);
     assert(answer == bt.getCommands(params));
@@ -20,7 +20,7 @@ Parameters params(colors);
 BTSerial bt(0, 0); // подключаем объект класса работы с блютуз
 int main() {
     const short N = 5;
-    char *inputs[N] = {"ON\n", "CON\n", "Con\ndfs", "HighAmp\n", "HighAm"};
+    const char *inputs[N] = {"ON\n", "CON\n", "Con\ndfs", "HighAmp\n", "HighAm"};
     short answers[N] = {ON, ERROR,OK, AMPLIFIER_ON, WAIT_INPUT};
     for (int i = 0; i != N; ++i) {
         tests(bt, params, inputs[i], answers[i]);
